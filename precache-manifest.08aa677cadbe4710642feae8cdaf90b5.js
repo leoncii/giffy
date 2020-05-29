a1e9bd44add20c6d28aabf8d788f20b7cf0b2fe9
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2020-05-28 21:50:47 -0400

    Updates

---
+++ precache-manifest.08aa677cadbe4710642feae8cdaf90b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b3ee6c1cfcda9b337199a13cdb8373",
+    "url": "/giffy/index.html"
+  },
+  {
+    "revision": "ce8e519dcd8d9d699ad2",
+    "url": "/giffy/static/css/main.840a2648.chunk.css"
+  },
+  {
+    "revision": "931292ead31ac95f8764",
+    "url": "/giffy/static/js/2.e59946f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giffy/static/js/2.e59946f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8e519dcd8d9d699ad2",
+    "url": "/giffy/static/js/main.38475436.chunk.js"
+  },
+  {
+    "revision": "f82b0107285b26b980e2",
+    "url": "/giffy/static/js/runtime-main.504f2b37.js"
+  }
+]);
